84b6f7249732638de98c05fde97fd8e1e34a9c75
David Sean <david.sean@gmail.com>
2023-09-13 13:25:25 -0400

    add alabaster theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,6 +25,7 @@ extensions = [
                 'sphinx.ext.autodoc',
                 'sphinx.ext.autosummary',
                 'sphinx.ext.napoleon',
+                'sphinx.ext.viewcode',
                 'myst_parser'
              ]
 
@@ -33,8 +34,5 @@ exclude_patterns = []
 
 
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
