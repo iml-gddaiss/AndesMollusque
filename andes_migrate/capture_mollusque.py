16fff56371c773c44f0c181203a5223af92008ef
David Sean <david.sean@gmail.com>
2023-09-22 09:25:11 -0400

    add to capture moll

--- andes_migrate/capture_mollusque.py
+++ andes_migrate/capture_mollusque.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 class CaptureMollusque(TablePecheSentinelle):
-    """ 
+    """
     Object model representing the CAPTURE_MOLLUSQUE table
     """
 
@@ -26,7 +26,7 @@ class CaptureMollusque(TablePecheSentinelle):
         self._init_rows()
 
     def _init_rows(self):
-        """ Initialisation method 
+        """Initialisation method
         This queries the Andes DB and creaters a list of row entries to be added to the current table
 
         After running this methods initialises the following attribute:
@@ -37,10 +37,12 @@ class CaptureMollusque(TablePecheSentinelle):
         self._row_idx will start at 0
 
         """
-        query = ("SELECT ecosystem_survey_catch.id "
-                "FROM ecosystem_survey_catch "
-               f"WHERE ecosystem_survey_catch.set_id={self.engin.trait._get_current_row_pk()} "
-                "ORDER BY ecosystem_survey_catch.id ASC;")
+        query = (
+            "SELECT ecosystem_survey_catch.id "
+            "FROM ecosystem_survey_catch "
+            f"WHERE ecosystem_survey_catch.set_id={self.engin.trait._get_current_row_pk()} "
+            "ORDER BY ecosystem_survey_catch.id ASC;"
+        )
 
         result = self.andes_db.execute_query(query)
         self._assert_not_empty(result)
@@ -49,22 +51,21 @@ class CaptureMollusque(TablePecheSentinelle):
         self._row_list = [catch[0] for catch in result]
         self._row_idx = 0
 
-
     def populate_data(self):
-        """_summary_
-        """
-        self.data['COD_SOURCE_INFO'] = self.get_cod_source_info()
-        self.data['COD_ENG_GEN'] = self.get_cod_eng_gen()
-        self.data['NO_RELEVE'] = self.get_no_releve()
-        self.data['COD_ESP_GEN'] = self.get_cod_esp_gen()
-        self.data['IDENT_NO_TRAIT'] = self.get_ident_no_trait()
-        self.data['COD_TYP_PANIER'] = self.get_cod_type_panier()
-        self.data['COD_NBPC'] = self.get_cod_nbpc()
-        self.data['FRACTION_PECH'] = self.get_fraction_peche()
-        # self.data['NO_ENGIN'] = self.get_
-        # self.data['FRACTION_ECH'] = self.get_
-        # self.data['COD_DESCRIP_CAPT'] = self.get_
-        # self.data['FRACTION_ECH_P'] = self.get_
+        """Populate data: run all getters"""
+
+        self.data["COD_SOURCE_INFO"] = self.get_cod_source_info()
+        self.data["COD_ENG_GEN"] = self.get_cod_eng_gen()
+        self.data["NO_RELEVE"] = self.get_no_releve()
+        self.data["COD_ESP_GEN"] = self.get_cod_esp_gen()
+        self.data["IDENT_NO_TRAIT"] = self.get_ident_no_trait()
+        self.data["COD_TYP_PANIER"] = self.get_cod_type_panier()
+        self.data["COD_NBPC"] = self.get_cod_nbpc()
+        self.data["FRACTION_PECH"] = self.get_fraction_peche()
+        self.data["NO_ENGIN"] = self.get_no_engin()
+        self.data["FRACTION_ECH"] = self.get_fraction_ech()
+        self.data['COD_DESCRIP_CAPT'] = self.get_cod_descrip_capt()
+        self.data['FRACTION_ECH_P'] = self.get_fraction_ech_p()
         # self.data['COD_TYP_MESURE'] = self.get_
         # self.data['NBR_CAPT'] = self.get_
         # self.data['FRACTION_PECH_P'] = self.get_
@@ -84,7 +85,7 @@ class CaptureMollusque(TablePecheSentinelle):
         """COD_SOURCE_INFO INTEGER / NUMBER(5,0)
         Identification de la source d'information tel que défini dans la table SOURCE_INFO
 
-        Extrait du trait::func:`~andes_migrate.trait_mollusque.TraitMollusque.get_cod_source_info`
+        Extrait de l'engin ::func:`~andes_migrate.engin_mollusque.EnginMollusque.get_cod_source_info`
 
         """
 
@@ -96,7 +97,8 @@ class CaptureMollusque(TablePecheSentinelle):
         """COD_ENG_GEN INTEGER / NUMBER(5,0)
         Identification de l'engin de pêche utilisé tel que défini dans la table ENGIN_GENERAL
 
-        Andes: shared_models_set.gear_type_id-> shared_models.geartype.code
+        Extrait de l'engin ::func:`~andes_migrate.engin_mollusque.EnginMollusque.get_cod_eng_gen`
+
         """
         return self.engin.get_cod_eng_gen()
 
@@ -105,14 +107,14 @@ class CaptureMollusque(TablePecheSentinelle):
         """NO_RELEVE INTEGER / NUMBER(5,0)
         Numéro séquentiel du relevé
 
-        Extrait du trait
-        """
+        Extrait de l'engin ::func:`~andes_migrate.engin_mollusque.EnginMollusque.get_no_releve`
+    """
         return self.engin.get_no_releve()
 
     @validate_int()
     @log_results
     def get_cod_esp_gen(self) -> int:
-        """ COD_ESP_GEN INTEGER / NUMBER(5,0)
+        """COD_ESP_GEN INTEGER / NUMBER(5,0)
         Identification de l'espèce capturée tel que défini dans la table ESPECE_GENERAL
 
         Associating this code to the andes species will require jumping through a number
@@ -125,19 +127,21 @@ class CaptureMollusque(TablePecheSentinelle):
         `COD_ESP_GEN` that is eventually mapped to a species code using the NORM (again,
         typically `IML_STRAP` or `RVAN`).
 
-        The link is initially made with the mapped values (ex., Andes.id->-strap = 
+        The link is initially made with the mapped values (ex., Andes.id->-strap =
         Oracle.id->strap) but then reversed to extract the needed `COD_ESP_GEN`.
-        
+
         If a species cannot be mapped to a pre-existing `COD_ESP_GEN`, an item is added to
         the tasks that need to be made prior to commiting the data.
 
 
         """
-        query = ("SELECT shared_models_species.id, shared_models_species.aphia_id, shared_models_species.code "
-                 "FROM ecosystem_survey_catch "
-                 "LEFT JOIN shared_models_species "
-                 "ON shared_models_species.id=ecosystem_survey_catch.species_id "
-                f"WHERE ecosystem_survey_catch.id={self._get_current_row_pk()}")
+        query = (
+            "SELECT shared_models_species.id, shared_models_species.aphia_id, shared_models_species.code "
+            "FROM ecosystem_survey_catch "
+            "LEFT JOIN shared_models_species "
+            "ON shared_models_species.id=ecosystem_survey_catch.species_id "
+            f"WHERE ecosystem_survey_catch.id={self._get_current_row_pk()}"
+        )
         result = self.andes_db.execute_query(query)
         self._assert_one(result)
 
@@ -147,46 +151,59 @@ class CaptureMollusque(TablePecheSentinelle):
         # print( andes_id, andes_aphia_id, andes_code)
 
         # try to match an aphia_id
-        self.logger.info("Will try to match to Oracle using aphia_id=%s", andes_aphia_id)
+        self.logger.info(
+            "Will try to match to Oracle using aphia_id=%s", andes_aphia_id
+        )
         norme_name_str = "AphiaId"
-        query = ("SELECT ESPECE_NORME.COD_ESP_GEN "
-                 "FROM ESPECE_NORME "
-                 "LEFT JOIN NORME "
-                 "ON ESPECE_NORME.COD_NORME=NORME.COD_NORME "
-                f"WHERE NORME.NOM_NORME='{norme_name_str}' "
-                f"AND ESPECE_NORME.COD_ESPECE={andes_aphia_id}")
+        query = (
+            "SELECT ESPECE_NORME.COD_ESP_GEN "
+            "FROM ESPECE_NORME "
+            "LEFT JOIN NORME "
+            "ON ESPECE_NORME.COD_NORME=NORME.COD_NORME "
+            f"WHERE NORME.NOM_NORME='{norme_name_str}' "
+            f"AND ESPECE_NORME.COD_ESPECE={andes_aphia_id}"
+        )
 
         result = self.reference_data.execute_query(query)
         try:
             self._assert_one(result)
         except ValueError:
-                self.logger.warn("did not match to Oracle using aphia_id=%s", andes_aphia_id)
+            self.logger.warn(
+                "did not match to Oracle using aphia_id=%s", andes_aphia_id
+            )
         else:
             to_return = result[0][0]
-            self.logger.info("Found matching aphia id, returning COD_ESP_GEN=%s", to_return)
+            self.logger.info(
+                "Found matching aphia id, returning COD_ESP_GEN=%s", to_return
+            )
             # return to_return
-        
+
         # try to match with (assumed) strap code
         self.logger.info("Will try to match to Oracle assuming strap=%s", andes_code)
         norme_name_str = "STRAP_IML"
-        query = ("SELECT ESPECE_NORME.COD_ESP_GEN "
-                 "FROM ESPECE_NORME "
-                 "LEFT JOIN NORME "
-                 "ON ESPECE_NORME.COD_NORME=NORME.COD_NORME "
-                f"WHERE NORME.NOM_NORME='{norme_name_str}' "
-                f"AND ESPECE_NORME.COD_ESPECE={andes_code}")
+        query = (
+            "SELECT ESPECE_NORME.COD_ESP_GEN "
+            "FROM ESPECE_NORME "
+            "LEFT JOIN NORME "
+            "ON ESPECE_NORME.COD_NORME=NORME.COD_NORME "
+            f"WHERE NORME.NOM_NORME='{norme_name_str}' "
+            f"AND ESPECE_NORME.COD_ESPECE={andes_code}"
+        )
 
         result = self.reference_data.execute_query(query)
         try:
             self._assert_one(result)
         except ValueError:
-            self.logger.warn("did not match to Oracle using code=%s (assuming strap)", andes_code)
-            raise(ValueError)
+            self.logger.warn(
+                "did not match to Oracle using code=%s (assuming strap)", andes_code
+            )
+            raise (ValueError)
         else:
             to_return = result[0][0]
-            self.logger.info("Found matching strap code, returning COD_ESP_GEN=%s", to_return)
+            self.logger.info(
+                "Found matching strap code, returning COD_ESP_GEN=%s", to_return
+            )
             return to_return
-
 
     @validate_int()
     @log_results
@@ -194,17 +211,20 @@ class CaptureMollusque(TablePecheSentinelle):
         """IDENT_NO_TRAIT INTEGER / NUMBER(5,0)
         Numéro séquentiel d'identification du trait
 
-        Extrait du trait
+        Extrait de l'engin ::func:`~andes_migrate.engin_mollusque.EnginMollusque.get_ident_no_trait`
         """
         return self.engin.get_ident_no_trait()
-
 
     @validate_int()
     @log_results
     def get_cod_type_panier(self) -> int:
         """COD_TYP_PANIER INTEGER / NUMBER(5,0)
         Identification du type de panier utilisé tel que défini dans la table TYPE_PANIER
+
+        Extrait de l'engin ::func:`~andes_migrate.engin_mollusque.EnginMollusque.get_cod_type_panier`
+
         """
+
         return self.engin.get_cod_type_panier()
 
     @log_results
@@ -212,21 +232,96 @@ class CaptureMollusque(TablePecheSentinelle):
         """COD_NBPC VARCHAR(6) / VARCHAR2(6)
         Numéro du navire utilisé pour réaliser le relevé tel que défini dans la table NAVIRE
 
-        Extrait du trait
+        Extrait de l'engin ::func:`~andes_migrate.engin_mollusque.EnginMollusque.get_cod_nbpc`
+
         """
         return self.engin.get_cod_nbpc()
 
     @log_results
     def get_fraction_peche(self) -> float:
-        """ FRACTION_PECH DOUBLE / NUMBER
+        """FRACTION_PECH DOUBLE / NUMBER
         Fraction de panier qui a effectivement pêché. Nb paniers qui ont pêché sur nb paniers installés
 
-        N.B. the description seems wrong, it's not the fraction baskets, rather the number of baskets
+        N.B. the description seems wrong, it's not the fraction of baskets, rather the number of baskets
         Oracle Optimisation: this datatype should be INTEGER
 
-        Andes
-        This is currently not recorded in Andes.
+        Andes: This is currently not recorded in Andes.
+        Hard-coded: This functino always returns 4
 
         """
-        to_return = 4
+
+        to_return = self._hard_coded_result(4)
         return to_return
+
+    @log_results
+    def get_no_engin(self) -> int:
+        """NO_ENGIN INTEGER/ NUMBER(5,0)
+        Numéro identifiant l'engin utilisé tel que défini dans la table NO_ENGIN
+
+        Extrait de l'engin ::func:`~andes_migrate.engin_mollusque.EnginMollusque.get_no_engin`
+
+        """
+
+        return self.engin.get_no_engin()
+
+    @log_results
+    def get_fraction_ech(self)-> float:
+        """FRACTION_ECH DOUBLE / NUMBER
+        Fraction échantillonné. Nb paniers échantillonnés sur Nb paniers total de l'engin de pêche
+
+        Andes: This is currently not recorded in Andes.
+
+        Hard-coded: This function always returns 4
+
+        """
+
+        to_return = self._hard_coded_result(4)
+        return to_return
+
+    @validate_int(not_null=False)
+    @log_results
+    def get_cod_descrip_capt(self) -> int| None:
+        """COD_DESCRIP_CAPT INTEGER / NUMBER(5,0)
+        Description qualitative de la capture tel que définie dans la table DESCRIP_CAPT_MOLL
+
+        0 -> Espèce absente dans la capture
+        1 -> Espèce présente dans la capture
+        2 -> Espèce abondante dans la capture
+        3 -> Espèce très abondante dans la capture
+
+        Andes: shared_models_relativeabundancecategory.code
+         via ecosystem_survey_catch.relative_abundance_category_id 
+
+        N.B. An effort has to be made to ensure the categories match between Andes and Oracle
+
+        This one would be good to have linked with a regional code lookup
+        https://github.com/dfo-gulf-science/andes/issues/988
+
+        Code lookup
+        """
+        query = (
+            "SELECT shared_models_relativeabundancecategory.code "
+            "FROM ecosystem_survey_catch "
+            "LEFT JOIN shared_models_relativeabundancecategory "
+            "ON shared_models_relativeabundancecategory.id=ecosystem_survey_catch.relative_abundance_category_id "
+            f"WHERE ecosystem_survey_catch.id={self._get_current_row_pk()}"
+        )
+        result = self.andes_db.execute_query(query)
+        self._assert_one(result)
+
+        to_return = result[0][0]
+        return to_return
+
+    @log_results
+    def get_fraction_ech_p(self) -> float | None:
+        """FRACTION_ECH_P DOUBLE / NUMBER 
+        Nombre de chiffre après la décimale pour la précision d'affichage associée à "Fraction_Ech"
+
+        Andes: This is currently not recorded in Andes.
+
+        Hard-coded: This function always returns None
+        """
+        return self._hard_coded_result(None)
+    
+
+    
